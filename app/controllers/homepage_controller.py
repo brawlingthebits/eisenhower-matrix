c13ed8388332384869eb6910331b3deb9236935e
Bernardo <bernardocardosodacosta@gmail.com>
2022-04-27 13:33:03 -0300

    feat: create get all categories tasks controller

--- app/controllers/homepage_controller.py
+++ app/controllers/homepage_controller.py
@@ -1,2 +1,34 @@
+from http import HTTPStatus
+from flask import jsonify
+from app.models.categories_model import CategoriesModel
+from app.models.tasks_model import TasksModel
+
 def all_tasks():
-    return "Todas as tasks"
+    categories = CategoriesModel.query.all()
+    result = []
+
+    for category in categories:
+        category_data = {
+            "id": category.id,
+            "name": category.name,
+            "description": category.description,
+            "tasks": []
+        }
+
+        for task in category.tasks:
+            importance = task.importance
+            urgency = task.urgency
+
+            task_data = {
+                "id": task.id,
+                "name": task.name,
+                "description": task.description,
+                "duration": task.duration,
+                "classification": TasksModel.eisenhower_type(importance, urgency)
+            }
+
+            category_data["tasks"].append(task_data)
+
+        result.append(category_data)
+
+    return jsonify(result), HTTPStatus.OK
